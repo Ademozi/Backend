2312ceeb296f65cf49034820d151931531b54e82
Ademozi <ademoviadem@gmail.com>
2026-07-08 10:55:45 +0100

    Path parameters and Body parameters

--- node-js-project/index.js
+++ node-js-project/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 // the function has two parameters (request, response)
 app.get("/hello", (req, res) => {
@@ -15,10 +16,6 @@ app.get("/hi", (req, res) => {
     res.send(`The numbers are ${numbers}`);
 })
 
-app.put("/test", (req, res) => {
-    res.send("hello world");
-})
-
 // When you visit the /addComment route, it will notwork because the method is POST, not GET.
 //  You can use Postman to test this route.
 // Typing the URL in the browser will not work because browsers only make GET requests when you type a URL in the address bar.
@@ -30,6 +27,38 @@ app.delete("/testingDelete", (req, res) => {
     res.send("delete request");
 })
 
+app.put("/test", (req, res) => {
+    res.send("hello world");
+})
+
+// ----Parameters in the URL---- 
+
+// Path parameters
+// :number1 & :number2 means you can ou anything in them
+app.get("/findSummation/:number1/:number2", (req, res) => {
+    const num1 = req.params.number1
+    const num2 = req.params.number2
+
+    const total = Number(num1) + Number(num2)
+
+    // console.log() run in the terminal because it is backend not frontend
+    console.log(req.params)
+    res.send(`the total is ${total}`)
+});
+
+// Body parameters
+app.get("/sayHello", (req, res) => {
+    console.log(req.body)
+
+    res.send(`Hello ${req.body.name}`)
+});
+
+// Query parameters
+
+
+
+
 app.listen(3000, () => {
     console.log("I am listening on port 3000")
 });
+// 54:13
