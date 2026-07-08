a12ea74622881ec4a2205495b95840333e633a67
Ademozi <ademoviadem@gmail.com>
2026-07-08 10:25:03 +0100

    Tesing different HTTP requests with Postman

--- node-js-project/index.js
+++ node-js-project/index.js
@@ -7,11 +7,27 @@ app.get("/hello", (req, res) => {
 })
 
 app.get("/hi", (req, res) => {
-    res.send("you visited hi");
+    let numbers = "";
+    for (let i = 0; i <= 100; i++) {
+        numbers += i + " - ";
+    }
+
+    res.send(`The numbers are ${numbers}`);
 })
 
-app.get("/test", (req, res) => {
-    res.send("you visited test");
+app.put("/test", (req, res) => {
+    res.send("hello world");
+})
+
+// When you visit the /addComment route, it will notwork because the method is POST, not GET.
+//  You can use Postman to test this route.
+// Typing the URL in the browser will not work because browsers only make GET requests when you type a URL in the address bar.
+app.post("/addComment", (req, res) => {
+    res.send("you posted to add comment");
+})
+
+app.delete("/testingDelete", (req, res) => {
+    res.send("delete request");
 })
 
 app.listen(3000, () => {
